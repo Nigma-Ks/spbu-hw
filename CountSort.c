868dbf734edcfc973a5f2831e9a4d99a76dcaa5b
Nigma-Ks <114099264+Nigma-Ks@users.noreply.github.com>
2022-10-01 23:22:56 +0500

    Change CountSort in solution2-3(\n, size of input)

--- CountSort.c
+++ CountSort.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+
 #define rangeOfSort 10001
 
 int main() {
-    int numbersForSort[rangeOfSort] = {0}, element = 0;
-    printf("This program sorts (from small to big) numbers (from -5000 to 5000) in entered array. Enter elements\n");
-    printf("The first element(when you fill your array, enter 5001): ");
-    scanf_s("%d", &element);
-    if (element == 5001) {
-        printf("Your array is empty");
-        return 0;
-    }
-    while (element != 5001) {
-        numbersForSort[element + 5000]++;
-        printf("The next element(when you fill your array, enter 5001): ");
+    int numbersForSort[rangeOfSort] = {0}, element = 0, amountOfElementsToSort = 0, amountOfEnteredElements = 0;
+    printf("This program sorts (from small to big) numbers (from -5000 to 5000) in entered array."
+           "\nEnter the amount of elements in array: ");
+    scanf_s("%d", &amountOfElementsToSort);
+    while (amountOfElementsToSort <= 0) {
+        printf("This program can't sort %d elements, try again.\nEnter the amount of elements in array: ",
+               amountOfElementsToSort);
         scanf_s("%d", &element);
+    }
+    while (amountOfEnteredElements != amountOfElementsToSort) {
+        printf("Enter element: ");
+        scanf_s("%d", &element);
+        if (element < -5000 || element > 5000) {
+            printf("Numbers have to be from -5000 to 5000, this number will be ignored, enter %d more elements\n",
+                   amountOfElementsToSort - amountOfEnteredElements);
+        } else {
+            numbersForSort[element + 5000]++;
+            amountOfEnteredElements++;
+        }
     }
     printf("Now your array looks like this: ");
     for (int i = 0; i < rangeOfSort; i++) {
@@ -21,5 +29,6 @@ int main() {
             printf("%d ", i - 5000);
         }
     }
+    printf("\n");
     return 0;
 }
