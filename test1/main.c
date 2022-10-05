e7f56f788cf7a0e9b1ff27d83d437e137fb3ad50
Nigma-Ks <114099264+Nigma-Ks@users.noreply.github.com>
2022-10-05 18:46:36 +0500

    +tests

--- test1/main.c
+++ test1/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define arraySize 200
+#define size 5
 
 void insertionSortOfEven(int arrayForSort[], int startIndex, int finishIndex) {
-    if (finishIndex - startIndex + 1 > 1) {
+    if ((finishIndex - startIndex + 1 > 1) && (startIndex >= 0) && (finishIndex >= 0)) {
         int copyOfValue = 0;
         for (int i = startIndex + 1; i <= finishIndex; i++) {
-            if (arrayForSort[i]%2 == 0) {
+            if (arrayForSort[i] % 2 == 0) {
                 int indexOfCurrentElement = i;
                 for (int j = i - 1; j >= startIndex; j--) {
                     if (arrayForSort[j] % 2 == 0) {
@@ -23,6 +25,61 @@ void insertionSortOfEven(int arrayForSort[], int startIndex, int finishIndex) {
             }
         }
     }
+}
+
+bool testWithZeroEvenNumbers() {
+    int arrayForSort[size] = {1, 3, 7, 9, -13};
+    int answer[size] = {1, 3, 7, 9, -13};
+    insertionSortOfEven(arrayForSort, 0, size - 1);
+    for (int i = 0; i < size; i++) {
+        if (answer[i] != arrayForSort[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool testWithOneEvenNumber() {
+    int arrayForSort[size] = {1, 2, 7, 9, -13};
+    int answer[size] = {1, 2, 7, 9, -13};
+    insertionSortOfEven(arrayForSort, 0, size - 1);
+    for (int i = 0; i < size; i++) {
+        if (answer[i] != arrayForSort[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool testWithTreeEvenNumber() {
+    int arrayForSort[size] = {-900, 88, 7, 66, -13};
+    int answer[size] = {-900, 66, 7, 88, -13};
+    insertionSortOfEven(arrayForSort, 0, size - 1);
+    for (int i = 0; i < size; i++) {
+        if (answer[i] != arrayForSort[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool testWithAllEvenNumber() {
+    int arrayForSort[size] = {-900, 88, 0, 66, -80};
+    int answer[size] = {-900, -80, 0, 66, 88};
+    insertionSortOfEven(arrayForSort, 0, size - 1);
+    for (int i = 0; i < size; i++) {
+        if (answer[i] != arrayForSort[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool testOfTests() {
+    if (testWithZeroEvenNumbers() && testWithOneEvenNumber() && testWithTreeEvenNumber() && testWithAllEvenNumber()) {
+        return true;
+    }
+    return false;
 }
 
 int main() {
@@ -45,6 +102,12 @@ int main() {
     for (int i = 0; i < amountOfElements; i++) {
         printf("%d ", arrayForSort[i]);
     }
+    if (testOfTests()) {
+        printf("\nAll tests are passed");
+    } else {
+        printf("\nTests are failed");
+    }
     printf("\n");
     return 0;
 }
+
