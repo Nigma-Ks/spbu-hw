ddaa6430c11a0170fd58e25a0f3efa5612316d06
Nigma-Ks <114099264+Nigma-Ks@users.noreply.github.com>
2022-10-18 23:40:48 +0500

    +the most repeated element

--- hw4-2/main.c
+++ hw4-2/main.c
@@ -1,32 +1,63 @@
 #include "sort.h"
 
-int main() {
-    FILE *file = fopen("C:\\Users\\Huawei\\Desktop\\HW\\spbu-hw\\hw4-2\\array", "r");
-    if (file == NULL) {
-        printf("file not found!");
-        return 0;
+
+void quickSort(int *arrayForSort, int startIndex, int finishIndex) {
+    if ((startIndex >= 0) && (finishIndex >= 0) && (finishIndex - startIndex + 1 > 1)) {
+        int firstIndexWithBigger = finishIndex;
+        int elementForComparison = arrayForSort[startIndex], copyOfValue = 0;
+        for (int i = startIndex + 1; i <= finishIndex; i++) {
+            if (i >= firstIndexWithBigger) break;
+            if (arrayForSort[i] >= elementForComparison) {
+                copyOfValue = arrayForSort[i];
+                arrayForSort[i] = arrayForSort[firstIndexWithBigger];
+                arrayForSort[firstIndexWithBigger] = copyOfValue;
+                firstIndexWithBigger--;
+                i--;
+            }
+        }
+        if (arrayForSort[firstIndexWithBigger] >= arrayForSort[startIndex]) {
+            copyOfValue = arrayForSort[startIndex];
+            arrayForSort[startIndex] = arrayForSort[firstIndexWithBigger - 1];
+            arrayForSort[firstIndexWithBigger - 1] = copyOfValue;
+            firstIndexWithBigger--;
+        } else {
+            copyOfValue = arrayForSort[startIndex];
+            arrayForSort[startIndex] = arrayForSort[firstIndexWithBigger];
+            arrayForSort[firstIndexWithBigger] = copyOfValue;
+        }
+        quickSort(arrayForSort, startIndex, firstIndexWithBigger - 1);
+        quickSort(arrayForSort, firstIndexWithBigger + 1, finishIndex);
     }
-    //C:\Users\Huawei\Desktop\HW\spbu-hw\hw4-2\array
-    int amountOfNumbers = amountOfNumbersInFile(file);
-    int *arrayForSort = calloc(amountOfNumbers, sizeof(int));
+}
+
+int amountOfNumbersInFile(FILE *file) {
+    int amount = 0, value = 0;
     fseek(file, 0, SEEK_SET);
-    int linesRead = 0;
     while (!feof(file)) {
-        if (fscanf(file, "%d", &arrayForSort[linesRead]) != 1) {
+        if (fscanf(file, "%d", &value) != 1) {
             break;
         }
-        ++linesRead;
+        amount++;
     }
-    fclose(file);
-    printf("Your array in file: ");
-    for (int i = 0; i < linesRead; i++) {
-        printf("%d ", arrayForSort[i]);
+    return amount;
+}
+
+int searchOfMostRepeatedElement(const int arrayWithNumbers[], int amountOfNumbers) {
+    int maxAmountOfRepeats = 1, mostRepeatedNumber = arrayWithNumbers[0], repeatsCounter = 1;
+    for (int j = 1; j < amountOfNumbers; ++j) {
+        if (arrayWithNumbers[j] != arrayWithNumbers[j - 1]) {
+            if (maxAmountOfRepeats < repeatsCounter) {
+                maxAmountOfRepeats = repeatsCounter;
+                mostRepeatedNumber = arrayWithNumbers[j - 1];
+            }
+            repeatsCounter = 1;
+        } else {
+            ++repeatsCounter;
+        }
     }
-    quickSort(arrayForSort, 0, amountOfNumbers - 1);
-    printf("\nYour sorted array: ");
-    for (int i = 0; i < linesRead; i++) {
-        printf("%d ", arrayForSort[i]);
+    if (maxAmountOfRepeats < repeatsCounter) {
+        maxAmountOfRepeats = repeatsCounter; //не используется, но если понадобится количество
+        mostRepeatedNumber = arrayWithNumbers[amountOfNumbers - 1]; //повторов, то оно в этой пременной
     }
-    printf("\n");
-    return 0;
+    return mostRepeatedNumber;
 }
