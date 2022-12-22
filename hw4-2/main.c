0ce8a82d2626f39361320bb097087e9e996ca779
Nigma-Ks <114099264+Nigma-Ks@users.noreply.github.com>
2022-12-23 01:46:38 +0500

    +tests

--- hw4-2/main.c
+++ hw4-2/main.c
@@ -1,7 +1,10 @@
-#include "sort.h"
 #include <stdlib.h>
+#include <stdbool.h>
+#include "sort.h"
 
 #define FILE_PATH "..\\array"
+#define TEST1_FILE_PATH "..\\test1_file"
+#define TEST2_FILE_PATH "..\\test2_file"
 
 int searchOfMostRepeatedElement(const int arrayWithNumbers[], int amountOfNumbers) {
     int maxAmountOfRepeats = 1, mostRepeatedNumber = arrayWithNumbers[0], repeatsCounter = 1;
@@ -39,7 +42,35 @@ void printError(int error) {
     printf("Memory error!\n");
 }
 
+bool testWithWrongFile() {
+    int error = 0, amountOfNumbers = 0;
+    FILE *testFile1 = fopen(TEST1_FILE_PATH, "a+");
+    fprintf(testFile1, "5\nk\n7");
+    fclose(testFile1);
+    int *testArrayForSort = arrayOfNumbersInFile(TEST1_FILE_PATH, &amountOfNumbers, &error);
+    remove(TEST1_FILE_PATH);
+    return error == -2;
+}
+
+bool testWithRightFile() {
+    int error = 0, amountOfNumbers = 0;
+    FILE *testFile2 = fopen(TEST2_FILE_PATH, "a+");
+    fprintf(testFile2, "5\n5\n7\n5\n7\n8\n90");
+    fclose(testFile2);
+    int *testArrayForSort = arrayOfNumbersInFile(TEST2_FILE_PATH, &amountOfNumbers, &error);
+    quickSort(testArrayForSort, 0, amountOfNumbers - 1);
+    int result = searchOfMostRepeatedElement(testArrayForSort, amountOfNumbers);
+    free(testArrayForSort);
+    remove(TEST2_FILE_PATH);
+    return result == 5;
+}
+
 int main() {
+    if (testWithWrongFile() && testWithRightFile()) {
+        printf("Tests were passed!\n");
+    } else {
+        printf("Tests weren't passed!\n");
+    }
     int amountOfNumbers = 0;
     int error = 0;
     int *arrayForSort = arrayOfNumbersInFile(FILE_PATH, &amountOfNumbers, &error);
