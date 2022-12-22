3459a20839f472cb42cb12a9fe51f561098b4521
Nigma-Ks <114099264+Nigma-Ks@users.noreply.github.com>
2022-12-22 22:18:59 +0500

    fixed qSort, binarySearch, index array's end

--- hw3_2/main.c
+++ hw3_2/main.c
@@ -6,17 +6,18 @@
 #define range 5000
 
 void quickSort(int arrayForSort[], int startIndex, int finishIndex) {
-    if ((startIndex >= 0) && (finishIndex >= 0) && (finishIndex - startIndex + 1 > 1)) {
+    if (finishIndex > startIndex && startIndex >= 0 && finishIndex >= 0) {
         int firstIndexWithBigger = finishIndex;
         int elementForComparison = arrayForSort[startIndex], copyOfValue = 0;
-        for (int i = startIndex + 1; i <= finishIndex; i++) {
-            if (i >= firstIndexWithBigger) break;
+        int i = startIndex + 1;
+        while (i <= finishIndex && i < firstIndexWithBigger) {
             if (arrayForSort[i] >= elementForComparison) {
                 copyOfValue = arrayForSort[i];
                 arrayForSort[i] = arrayForSort[firstIndexWithBigger];
                 arrayForSort[firstIndexWithBigger] = copyOfValue;
                 firstIndexWithBigger--;
-                i--;
+            } else {
+                i++;
             }
         }
         if (arrayForSort[firstIndexWithBigger] >= arrayForSort[startIndex]) {
@@ -37,20 +38,16 @@ void quickSort(int arrayForSort[], int startIndex, int finishIndex) {
 bool binarySearch(const int arrayInWhichFind[], int amountOfElementsInArray, int elementToFind) {
     int startIndex = 0, finishIndex = amountOfElementsInArray, currIndex = 0;
     while (startIndex != finishIndex) {
-        currIndex = (int) (finishIndex - startIndex) / 2 + startIndex;
+        currIndex = (finishIndex + startIndex) / 2;;
         if (elementToFind > arrayInWhichFind[currIndex]) {
-            if (startIndex == currIndex) break; //избегаем ситуации бесконечного цикла
-            startIndex = currIndex;
+            startIndex = currIndex + 1;
         } else if (elementToFind < arrayInWhichFind[currIndex]) {
-            finishIndex = currIndex;
+            finishIndex = currIndex - 1;
         } else {
             return true;
         }
     }
-    if (arrayInWhichFind[finishIndex] == elementToFind) { //finish потому что округление вниз,
-        return true;
-    }
-    return false;
+    return arrayInWhichFind[finishIndex] == elementToFind;
 }
 
 int main() {
@@ -74,10 +71,10 @@ int main() {
     int *arrayWithElementsToFind = (int *) calloc(amountOfElementsToFind, sizeof(int));
     printf("Your array with n elements: ");
     for (int i = 0; i < amountOfElements; i++) {
-        arrayInWhichFind[i] = rand() % (range + 1) - (int) range / 2;
+        arrayInWhichFind[i] = rand() % (range + 1) - range / 2;
         printf("%d ", arrayInWhichFind[i]);
     }
-    quickSort(arrayInWhichFind, 0, amountOfElements);
+    quickSort(arrayInWhichFind, 0, amountOfElements - 1);
     printf("\nYour k elements: \n");
     for (int j = 0; j < amountOfElementsToFind; j++) {
         arrayWithElementsToFind[j] = rand() % (range + 1) - (int) range / 2;
